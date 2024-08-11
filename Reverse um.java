535a68e3aef68517b390a238daa79239c6d7e6e8
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-08-11 18:33:00 +0530

    Update

---
+++ Reverse um.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+        int number = 4321;
+        String s = "";
+        while(number!=0){
+int last= number%10;
+s= s+Integer.toString(last);
+number =number /10;
+        }
+        
+        System.out.print(s);
+        
+    }
+}
