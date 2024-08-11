1536326ef451dee32bce767df7f61112f9f8b36a
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-08-11 18:48:20 +0530

    Fabinocii Series in java

---
+++ Fabinocii.java
@@ -0,0 +1,20 @@
+class Fabinocci {
+    public static void main(String[] args) {
+        
+        // fabinocii series upto 15
+        int range = 10;
+        int first = 0;
+        int second = 1;
+        int nextTerm;
+        System.out.print(first + " "+ second); 
+        for(int i=2;i<=range;i++)
+        {
+            nextTerm = first + second;
+            first = second;
+            second = nextTerm;
+  System.out.print(" " + nextTerm);
+        }
+      
+        
+    }
+}
