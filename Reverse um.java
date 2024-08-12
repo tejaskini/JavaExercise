30edd07634de053ca825cbc1aafdbc1ae25a79cc
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-08-12 14:34:52 +0530

    Update Reverse um.java

--- Reverse um.java
+++ Reverse um.java
@@ -3,15 +3,16 @@ class ReverseNum{
 //file name is ReverseNum.java
 
     public static void main(String[] args) {
+        
         int number = 4321;
+        
         String s = "";
+        
         while(number!=0){
-int last= number%10;
-s= s+Integer.toString(last);
-number =number /10;
+            int last = number % 10;
+            s = s + Integer.toString(last);
+            number = number / 10;
         }
-        
         System.out.print(s);
-        
     }
 }
