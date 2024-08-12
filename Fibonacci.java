a9d04c38798576211526dd816395193abca52db9
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-08-12 14:32:43 +0530

    Update Fibonacci.java
    You can also get range from user input just removing the hardcoded range

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
 class Fibonacci {
-// sorry for spell mistake 
+        // print the Fibonacci series upto 10
+    // Series is like : 0 1 1 2 3 5 8 13 .....
+    
     public static void main(String[] args) {
+
+        // You can also take i/p for specific range
+        /*
+        Scanner s = new Scanner(System.in);
+        int range = s.nextInt();
         
-        // fabinocii series upto 15
-        int range = 10;
-        int first = 0;
-        int second = 1;
-        int nextTerm;
+        */
+        
+// Here I hardcoded the range as 10
+        int range = 10; // specify the range
+        int first = 0;    // first number
+        int second = 1;    // Second number
+        int nextTerm;    // Variable for storing next number
         System.out.print(first + " "+ second); 
         for(int i=2;i<=range;i++)
         {
