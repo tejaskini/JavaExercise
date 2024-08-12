d3ccb0ee15f6176c47c2afcd982e55165e2be08f
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-08-12 14:34:07 +0530

    Update PerfectNumber.java

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -1,17 +1,24 @@
 class PerfectNumber{
+    
     public static void main(String[] args) {
+        
         int n= 7;
+        
         int sum =0;
-        for(int i=1;i<n;i++)
-        {
-            if(n%i ==0){
-                sum +=i;
+        
+        for(int i=1;i<n;i++){
+            
+            if(n % i == 0){
+                sum += i;
             }
         }
-        if(sum == n){
+        if( sum == n){
+            
             System.out.println("is perfect Number");
-        }else{
-System.out.println("is not perfect Number");
+            
+        }
+        else{
+            System.out.println("is not perfect Number");
         }
     }
 }
