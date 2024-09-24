eddf0709a62295479f20f780b7d30c537797c8f3
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-09-24 13:37:13 +0530

    Create Dev2.java

---
+++ Interface/Dev2.java
@@ -0,0 +1,11 @@
+package com.tejas.abstraction.intefaces;
+
+public abstract class Dev2 extends Dev1 {
+
+	@Override
+	public void withdraw() {
+		// TODO Auto-generated method stub
+		System.out.println(" You withdraw  " + 500);
+	}
+
+}
