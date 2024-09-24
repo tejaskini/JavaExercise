da2f542fe4a9d69384073a7bce9613acc934e2ae
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-09-24 13:36:20 +0530

    Create Bank.java
    Bank.java is interface with the services

---
+++ Interface/Bank.java
@@ -0,0 +1,14 @@
+//package com.tejas.abstraction.intefaces;
+
+interface Bank {
+	// interface contains all the services
+	public abstract void deposite();
+
+	void withdraw();
+
+	void loan();
+
+	void account();
+
+	void balanced();
+}
