c0969aa844b71ea465efb47ad957f12cc9f3e42c
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-09-24 13:29:42 +0530

    Create BankRunner.java
    It is the main class

---
+++ Interface/BankRunner.java
@@ -0,0 +1,15 @@
+//package com.tejas.abstraction.intefaces;
+
+public class BankRunner {
+
+	public static void main(String[] args) {
+
+		Dev3 d = new Dev3();
+		d.deposite();
+		d.withdraw();
+		d.loan();
+		d.account();
+		d.balanced();
+	}
+
+}
