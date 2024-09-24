992cde3d4bbeb6b138f5c54901d21a75e8300590
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-09-24 13:36:49 +0530

    Create Dev1.java

---
+++ Interface/Dev1.java
@@ -0,0 +1,12 @@
+//package com.tejas.abstraction.intefaces;
+
+public abstract class Dev1 implements Bank {
+
+	@Override
+	public void deposite() {
+		// TODO Auto-generated method stub
+		System.out.println(" Your Deposite amount is  " + 1600);
+
+	}
+
+}
