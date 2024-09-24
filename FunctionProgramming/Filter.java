cf185b86c083b32f8fa456d49b0cfcb50d99e6a3
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-09-24 16:07:32 +0530

    Filter operation in FP

---
+++ FunctionProgramming/Filter.java
@@ -0,0 +1,36 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+class Filter {
+    public static void main(String[] args) {
+       List<String> list = new LinkedList<>();
+       list.add("Basic Operation using Functional Programming");
+
+    //filtering
+    list.stream().filter(
+        e->e.endsWith("on")
+        ).forEach(
+            e-> System.out.println(e)
+            );
+            
+    // print even number using FP
+    List<Integer> numList = List.of(1,3,4,5,96,7,9,2,55);
+    
+    System.out.print(numList);
+    numList.stream().filter(
+        element -> element%2 == 0 
+        ).forEach(
+            element -> System.out.println( element)
+            );
+
+  // Basic method to print even numbers
+  for(int i=0 i<numList.length; i++){
+    if(i %2 ==0)
+    {
+      System.out.print(i);
+    }
+  }
+
+    }
+}
