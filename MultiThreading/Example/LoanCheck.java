f0967d2dbd48898504fb99d0c66ee88b0a32f1cb
Tejas Kini <128882705+tejaskini@users.noreply.github.com>
2024-10-16 13:32:40 +0530

    Create LoanCheck.java

---
+++ MultiThreading/Example/LoanCheck.java
@@ -0,0 +1,12 @@
+package MultiThreading;
+
+public class LoanCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AmountApprove bank = new AmountApprove();
+		bank.start();
+
+	}
+
+}
